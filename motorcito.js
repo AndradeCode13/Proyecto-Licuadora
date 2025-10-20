94b6abf31dde12c1e71e7c5ca0bcb47e4ae64b5d
AndradeCode13 <vanesa2007ortega.200@gmail.com>
2025-10-19 21:08:20 -0500

    Add files via upload

---
+++ motorcito.js
@@ -0,0 +1,30 @@
+/* funcionamiento de la licuadora */
+var estadolicuadora = "apagada";
+var sonidolicuadora = document.getElementById("blender-sound");
+var botonlicuadora = document.getElementById("blender-button-sound");
+var licuadora = document.getElementById("blender");
+
+function controlarlicuadora() {
+    if (estadolicuadora == "apagada") {
+        estadolicuadora = "encendido";
+        hacerBrrBrr();
+        licuadora.classList.add("active");
+        /*console.log("me prendiste");*/
+    } else {
+        estadolicuadora = "apagada"
+        hacerBrrBrr();
+        licuadora.classList.remove("active");
+        /*console.log("me apagaste");*/
+    }
+}
+
+function hacerBrrBrr(){
+    if( sonidolicuadora.paused ) {
+        botonlicuadora.play();
+        sonidolicuadora.play();
+    } else {
+        botonlicuadora.play();
+        sonidolicuadora.pause();
+        sonidolicuadora.currentTime = 0;
+    }
+}
